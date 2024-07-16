2f64471ff840a357b4cf56f5cd711ff77fc9e1b9
mattiasson.sofia@icloud.com <mattiasson.sofia@icloud.com>
2024-07-16 23:36:00 +0200

    Add frame motion trasition to the hamburger menu

--- client/src/component/header/NavbarMobile.tsx
+++ client/src/component/header/NavbarMobile.tsx
@@ -1,9 +1,9 @@
 import { useRef, useState } from 'react'
-import { NavLink } from 'react-router-dom'
 import { Sling as Hamburger } from 'hamburger-react'
 import { useClickAway } from 'react-use'
 import { navRoutes } from '../../config/MenuItemsData'
 import MenuItems from './MenuItems'
+import { AnimatePresence, motion } from 'framer-motion'
 
 const NavbarMobile = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -16,22 +16,32 @@ const NavbarMobile = () => {
       <div className='z-50'>
         <Hamburger toggled={isOpen} size={40} toggle={setIsOpen} />
       </div>
-
-      {isOpen && (
-        <div className='fixed h-full bg-dark-green left-0 lg:shadow-4xl right-0 pt-5 '>
-          <ul className='grid gap-12'>
-            {navRoutes.map((menu, index) => {
-              return (
-                <MenuItems
-                  key={index}
-                  items={{ ...menu, subMenu: menu.subMenu || [] }}
-                  depthLevel={0}
-                />
-              )
-            })}
-          </ul>
-        </div>
-      )}
+      <AnimatePresence>
+        {isOpen && (
+          <>
+            <motion.div
+              initial={{ scale: 0, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              transition={{
+                type: 'spring',
+                stiffness: 260,
+                damping: 20,
+              }}
+              exit={{ opacity: 0, x: -100 }}
+              className='fixed h-full bg-dark-green left-0 lg:shadow-4xl right-0 pt-5'>
+              <ul className='grid gap-12'>
+                {navRoutes.map((menu, index) => (
+                  <MenuItems
+                    key={index}
+                    items={{ ...menu, subMenu: menu.subMenu || [] }}
+                    depthLevel={0}
+                  />
+                ))}
+              </ul>
+            </motion.div>
+          </>
+        )}
+      </AnimatePresence>
     </div>
   )
 }
