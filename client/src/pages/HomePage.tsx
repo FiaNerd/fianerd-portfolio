ec7fd8860fce904e800208e7027b0b30bc5c5035
mattiasson.sofia@icloud.com <mattiasson.sofia@icloud.com>
2024-09-04 01:31:33 +0200

    Add global styling for h1,h2,h3 and p tag

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ const HomePage = () => {
   const { t } = useTranslation('home');
   return (
     <>
-      <h1 className='bg-primary text-primary md:text-4xl font-heading font-black'>
+      <h1>
         {t('title')}
       </h1>
       <h2>{t('intro')}</h2>
