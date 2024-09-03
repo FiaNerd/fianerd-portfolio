22e749c365590e775c77143745324b80f8a4cd6b
mattiasson.sofia@icloud.com <mattiasson.sofia@icloud.com>
2024-09-03 22:30:01 +0200

    Fixed the title language for homepage

--- client/public/assets/i18n/translation.ts
+++ client/public/assets/i18n/translation.ts
@@ -1,7 +1,7 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector'; // Import LanguageDetector module
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import Backend from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector' // Import LanguageDetector module
 
 i18n
   .use(Backend)
@@ -14,8 +14,10 @@ i18n
       escapeValue: false,
     },
     backend: {
-      loadPath: 'assets/locales/{{lng}}/translation.json', 
+      loadPath: '/assets/locales/{{lng}}/{{ns}}.json',
     },
-  });
+    ns: ['homepage'],
+    defaultNS: 'homepage',
+  })
 
-export default i18n;
+export default i18n
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,8 +1,11 @@
+import { useTranslation } from "react-i18next"
+
 const HomePage = () => {
+  const { t } = useTranslation('home')
   return (
     <>
       <h1 className='md:text-4xl font-heading font-black'>
-        The Story of My Life
+        {t('title')}
       </h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam perspiciatis alias a doloremque! Ipsam sed deserunt aliquam ab consectetur veniam fugiat quasi iste fugit, distinctio, et aperiam? Voluptatem, odit incidunt.</p>
     </>
