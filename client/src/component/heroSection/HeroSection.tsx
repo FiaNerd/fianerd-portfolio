3c315a9ec96c741fdfaacf9db4d0f75ba0a9547b
Sofia Mattiasson <mattiasson.sofia@gmail.com>
2024-09-30 23:59:17 +0200

    Add background to the hero image

--- client/src/component/heroSection/HeroSection.tsx
+++ client/src/component/heroSection/HeroSection.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next"
 function HeroSection() {
     const { t } = useTranslation('heroSection')
   return (
-    <div className="grid grid-cols-2 gap-4 bg-cover dark:bg-[url('/assets/images/bg-img.jpg')] ">
+    <div className="grid grid-cols-2 gap-4 bg-cover bg-[url('/assets/images/bg-light-img.jpg')] dark:bg-[url('/assets/images/bg-dark-img.jpg')] ">
        <div className="flex flex-col justify-center mx-auto p-12 gap-8">
        <h1>{t('title')}</h1>
        <h3>{t('subtitle')}</h3>
