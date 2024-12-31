00f83c8d79482728680146d8b9a4e49314969150
Sofia Mattiasson <mattiasson.sofia@gmail.com>
2024-12-31 14:10:43 +0100

    Add thank you text and styling for my profile

--- src/component/hobbies/Hobbies.tsx
+++ src/component/hobbies/Hobbies.tsx
@@ -16,28 +16,34 @@ const Hobbies = () => {
             />
             <p className='md:text-xl px-4 pb-8 justify-center mx-auto max-w-[80%] text-text-secondary'>{t('introHobbie')}</p>
 
-            <div className="grid grid-rows-1 sm:grid-cols-1 xl:grid-cols-3 gap-4 px-4 xl:px-20 text-center mb-12 jusity-center">
+            <div className="grid grid-rows-1 sm:grid-cols-1 xl:grid-cols-3 gap-4 px-4 xl:px-20 text-center mb-12 jusity-center ">
                 {/* Musik */}
-                <div className="lg:col-start-1 lg:row-start-2">
-                    <p className='font-textImportant text-xl md:text-4xl bg-slate-900 text-bg-primary dark:bg-accent-primary py-4 px-4'>
+                <div className="lg:col-start-1 lg:row-start-2 ">
+                    <p className='font-textImportant text-xl md:text-4xl bg-slate-900 text-bg-primary dark:bg-accent-primary py-2 md:py-4 px-4 border-2 dark:border-accent-primary rounded '>
                         {t("music")}
                     </p>
                 </div>
                 
                 {/* Konsert & Festival */}
                 <div className="lg:col-start-2 lg:row-start-1">
-                    <p className='font-textImportant text-xl md:text-4xl bg-slate-900 text-bg-primary dark:bg-accent-primary py-4 px-4'>
+                    <p className='font-textImportant text-xl md:text-4xl bg-slate-900 text-bg-primary dark:bg-accent-primary py-2 md:py-4 px-4 border-2 rounded dark:border-accent-primary'>
                         {t("consertAndFestival")}
                     </p>
                 </div>
                 
                 {/* Film & Serier */}
                 <div className="lg:col-start-3 lg:row-start-3">
-                    <p className='font-textImportant text-xl md:text-4xl bg-slate-900 text-bg-primary dark:bg-accent-primary py-4 px-4'>
+                    <p className='font-textImportant text-xl md:text-4xl bg-slate-900 text-bg-primary dark:bg-accent-primary py-2 md:py-4 px-4 border-2 rounded dark:border-accent-primary'>
                         {t("filmAndSeries")}
                     </p>
                 </div>
             </div>
+
+          <div className='max-w-[80%] mx-auto'>
+            <h2 className='font-textImportant text-6xl md:text-8xl text-center'>{t('thanksHobbie')}</h2>
+            <h2 className='font-textImportant text-4xl md:text-6xl text-center'>{t("thanksIntro")}</h2>
+            <p className='font-sub-heading text-center '>{t('thanksEnd')}</p>
+          </div>
         </>
     );
 };
