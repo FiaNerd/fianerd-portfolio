75c8d60ade3a89f89eb9d349c09dbc42d7afaf12
Sofia Mattiasson <mattiasson.sofia@gmail.com>
2025-01-04 00:24:20 +0100

    Add title goals for the web skills and graphic skill and chaged the layout for the goals

--- src/component/Profile/skills/GraphicSkills.tsx
+++ src/component/Profile/skills/GraphicSkills.tsx
@@ -24,7 +24,7 @@ const GraphicSkills = () => {
         className="grid grid-rows-[auto_auto] grid-cols-1 md:grid-cols-2 gap-20 mb-8"
       >
         {/* H1 in the first row, first column */}
-        <h1 className="text-center text-hover-text text-2xl md:text-5xl bg-[#4b1718]  dark:bg-bg-secondary dark:text-[#4b1718] py-2 md:py-4 px-4 md:col-start-1 md:row-start-1">
+        <h1 className="text-center text-hover-text text-2xl md:text-3xl lg:text-5xl bg-[#4b1718]  dark:bg-bg-secondary dark:text-[#4b1718] py-2 md:py-4 px-4 md:col-start-1 md:row-start-1">
           {t('graphicTitle')}
         </h1>
 
@@ -38,7 +38,7 @@ const GraphicSkills = () => {
         </div>
 
         <div className="items-center md:row-start-2 md:col-start-2">
-          <h1 className="text-[#4b1718] font-textImportant text-4xl md:text-5xl dark:text-bg-secondary font-bold text-center mb-2 whitespace-break-spaces">
+          <h1 className="text-[#4b1718] dark:text-bg-secondary font-textImportant text-4xl md:text-5xl font-bold text-center mb-2 whitespace-break-spaces">
             {t("titleGraphic")}
           </h1>
           <h3 className="text-center text-text-secondary not-italic font-light whitespace-break-spaces ">
@@ -54,30 +54,31 @@ const GraphicSkills = () => {
         initial="hidden"
         animate={ctrls}
         variants={vars}
-        className="grid grid-cols-1 md:grid-cols-[5%_50%_auto] gap-4 mb-8"
+        className="grid grid-cols-1 lg:grid-cols-[5%_50%_auto] gap-4 mb-8"
       >
 
         
         {/* Vertical Text Column */}
-        <div className="flex items-center justify-center">
+        <div className="items-center justify-center hidden lg:block">
           <h1 className="text-[#4b1718] dark:text-bg-secondary font-semibold tracking-widest transform rotate-180 [writing-mode:vertical-lr] text-center">
             {t("graphicTitle").toUpperCase()}
           </h1>
         </div>
 
         {/* Middle Column */}
-        <div className="sm:col-span-1 md:pr-8 flex flex-col justify-between	">
+        <div className="sm:col-span-1 order-last lg:order-none md:pr-8 flex flex-col justify-between	">
+           <p className="italic font-medium font-sub-heading underline decoration-wavy text-[#4b1718] dark:text-bg-secondary dark:decoration-text-secondary underline-offset-8 text-md md:text-xl mb-6">{t('textTitleGraphicGoal')}</p>
           <p className="text-text-sub-heading not-italic" dangerouslySetInnerHTML={{ __html: t("textPS") }}></p>
           <p className="text-text-primary" dangerouslySetInnerHTML={{ __html: t("textAI") }}></p>
           <p className="text-text-primary" dangerouslySetInnerHTML={{ __html: t("textID") }}></p>
-          <p className="text-text-primary" dangerouslySetInnerHTML={{ __html: t("textUX/UI") }}></p>
-          <p className="text-hover-text dark:text-bg-primary italic font-normal py-2 px-4 bg-[#4b1718] dark:bg-bg-secondary">
-            {t("textGoal")}
+          <p className="text-text-primary mb-12" dangerouslySetInnerHTML={{ __html: t("textUX/UI") }}></p>
+          <p className="text-[#4b1718] dark:text-text-secondary font-sub-heading outline-dashed outline-2 outline-[#4b1718] dark:outline-bg-secondary outline-offset-4 italic font-normal py-2 px-4 p-2 rounded-md">
+            {t('textGoal')}
           </p>
         </div>
 
         {/* Skills Column */}
-        <div className="col-span-2 md:col-span-1">
+        <div className="col-span-2 md:col-span-1 order-first lg:order-none">
           
           {skillsGraphic.map((skill) => (
             <div key={skill.name}>
--- src/component/Profile/skills/WebSkills.tsx
+++ src/component/Profile/skills/WebSkills.tsx
@@ -32,7 +32,7 @@ const WebSkills = () => {
         className="grid grid-rows-[auto_auto] grid-cols-1 md:grid-cols-2 gap-20 mb-8"
       >
 
-        <h1 className="text-center text-hover-text text-2xl md:text-3xl bg-[#535437] dark:bg-[#69864e] dark:text-[#4b1718]  py-2 md:py-4 px-4 md:col-start-1 md:row-start-1">
+        <h1 className="text-center text-hover-text text-2xl md:text-3xl lg:text-5xl bg-[#535437] dark:bg-[#69864e] dark:text-[#4b1718]  py-2 md:py-4 px-4 md:col-start-1 md:row-start-1">
           {t('webbDev')}
         </h1>
 
@@ -61,17 +61,18 @@ const WebSkills = () => {
           initial="hidden" 
           animate={ctrls} 
           variants={vars} 
-          className="grid grid-cols-1 md:grid-cols-[5%_50%_auto] gap-8 mb-8"
+          className="grid grid-cols-1 md:order-0 lg:grid-cols-[5%_50%_auto] gap-8 mb-8"
         >
           {/* Vertical Text Column */}
-          <div className="flex items-center justify-center">
-            <h1 className="uppercase font-semibold whitespace-pre-line text-[#535437] dark:text-[#69864e] tracking-widest transform rotate-180 [writing-mode:vertical-lr] text-center">
+          <div className="hidden lg:block items-center justify-center">
+            <h1 className="uppercase font-semibold font-sub-heading whitespace-pre-line text-[#535437] dark:text-[#69864e] tracking-widest transform rotate-180 [writing-mode:vertical-lr] text-center">
               {t('webbDev')}
             </h1>
           </div>
 
         {/* Middle Column */}
-        <div className="sm:col-span-1 md:pr-8">
+        <div className="sm:col-span-1 order-last lg:order-none md:pr-8">
+          <p className="italic font-medium underline decoration-wavy text-center underline-offset-8 derocation-text-secondary dark:decoration-text-secondary text-md md:text-2xl md:mb-8 text-[#535437] dark:text-[#69864e]">{t('textTitleDevGoal')}</p>
           <h5 className="text-text-secondary not-italic mb-0" dangerouslySetInnerHTML={{ __html: t('titleBE') }}></h5>
           <p dangerouslySetInnerHTML={{ __html: t('textBE') }}></p>
           <p dangerouslySetInnerHTML={{ __html: t('textDB') }}></p>
@@ -80,10 +81,11 @@ const WebSkills = () => {
           <p className="text-text-primary" dangerouslySetInnerHTML={{ __html: t('textFE') }}></p>
           <p className="text-text-primary" dangerouslySetInnerHTML={{ __html: t('textVue') }}></p>
           <p className="text-text-primary" dangerouslySetInnerHTML={{ __html: t('textCMS') }}></p>
-          <p className="text-text-primary mg-8" dangerouslySetInnerHTML={{ __html: t('textAzure') }}></p>
-          <p className="text-hover-text dark:text-bg-primary italic font-normal py-2 px-4 bg-[#535437] dark:bg-[#69864e] p-2 rounded-md">
+          <p className="text-text-primary mb-12" dangerouslySetInnerHTML={{ __html: t('textAzure') }}></p>
+          <p className="text-text-secondary font-sub-heading outline-dashed outline-2 outline-text-secondary dark:outline-[#69864e] outline-offset-4 italic font-normal py-2 px-4 p-2 rounded-md">
             {t('textGoal')}
           </p>
+
         </div>
 
         {/* Skills Column */}
