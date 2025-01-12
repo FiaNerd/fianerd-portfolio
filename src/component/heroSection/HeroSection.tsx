6fa0658f1b7788b6c8e52ff3130b58a71ad2a614
Sofia Mattiasson <mattiasson.sofia@gmail.com>
2025-01-12 21:15:00 +0100

    Better responsive text in the hero section

--- src/component/heroSection/HeroSection.tsx
+++ src/component/heroSection/HeroSection.tsx
@@ -23,25 +23,24 @@ const HeroSection = () => {
 
   return (
     <div className="relative w-full h-full flex justify-center">
-      <div className="absolute grid grid-cols-1 md:grid-cols-[80%] 2xl:grid-cols-[50%] w-full h-full z-10">
+      <div className="absolute grid grid-cols-1 md:grid-cols-[80%] lg:grid-cols-[60%] 2xl:grid-cols-[50%] w-full h-full z-10">
         {/* Text Section */}
-        <div className="flex flex-col justify-center items-start px-6 md:px-12 text-left md:text-left z-10 ">
+        <div className="flex flex-col justify-center items-start px-6 md:px-12 text-left md:text-left z-10 w-full h-full">
           <div className="bg-gray-300/40 dark:bg-black/40 p-4 rounded text-text-accent ">
-            <h1 className="text2xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-4xl mb-0 font-bold leading-snug" dangerouslySetInnerHTML={{ __html: t("title") }} />
-            <h2 className="text-[#350712] dark:text-accent-primary text-xl sm:text-2xl md:text-2xl lg:text-3xl mt-4 mb-0">{t("subTitle")}</h2>
-            <h3 className="text-[#350712]/80 text-lg sm:text-xl md:text-xl ">{t("textIntro")}</h3>
-            <p className="text-[#350712] text-sm sm:text-base md:text-md ">{t("text")}</p>
-          {/* Buttons */}
-          <div className="flex flex-col md:flex-row gap-4">
-            <Button className="bg-btn-bg text-bg-primary border-2 border-btn-bg hover:border-bg-hover hover:bg-bg-hover">
-              {t("ctaButtonCV")}
-            </Button>
-            <Button className="bg-transparent border-2 text-[#350712] border-[#350712] hover:bg-[#350712] hover:text-hover-text">
-              {t("ctaButtonPortfolio")}
-            </Button>
+            <h1 className="text-2xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-4xl mb-4 font-bold leading-snug lg:mb-12" dangerouslySetInnerHTML={{ __html: t("title") }} />
+            <h2 className="text-[#350712] dark:text-accent-primary text-xl sm:text-2xl md:text-2xl lg:text-3xl mt-4 mb-4">{t("subTitle")}</h2>
+            <h3 className="text-[#350712]/80 dark:text-text-accent text-lg sm:text-xl md:text-xl mb-4">{t("textIntro")}</h3>
+            <p className="text-[#350712] dark:text-text-primary text-sm sm:text-base md:text-md mb-4 lg:mb-8">{t("text")}</p>
+            {/* Buttons */}
+            <div className="flex flex-col md:flex-row gap-8">
+              <Button className="bg-btn-bg text-bg-primary border-2 border-btn-bg hover:border-bg-hover hover:bg-bg-hover">
+                {t("ctaButtonCV")}
+              </Button>
+              <Button className="bg-transparent border-2 text-[#350712] dark:text-btn-bg border-[#350712] dark:border-btn-bg hover:bg-bg-hover hover:border-bg-hover hover:text-bg-primary dark:hover:text-bg-primary">
+                {t("ctaButtonPortfolio")}
+              </Button>
+            </div>
           </div>
-          </div>
-
         </div>
       </div>
 
