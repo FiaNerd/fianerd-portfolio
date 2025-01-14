a38552677ee6e272f1585ff38bb04bfef96fb959
FiaNerd <mattiasson.sofia@gicloud.com>
2025-01-14 22:18:03 +0100

    Add favi icon to the web browser tab with dark icon switch light icon

--- src/component/ThemeSwitch.tsx
+++ src/component/ThemeSwitch.tsx
@@ -1,6 +1,6 @@
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { ThemeContext } from '../context/ThemeContext';
 
 const ThemeSwitch: React.FC = () => {
@@ -12,12 +12,27 @@ const ThemeSwitch: React.FC = () => {
 
   const { currentTheme, toggleTheme } = themeContext;
 
+  useEffect(() => {
+    const favicon = document.getElementById('favicon') as HTMLLinkElement;
+
+    if (!favicon) {
+      console.error('Favicon element not found');
+      return;
+    }
+
+    if (currentTheme === 'dark') {
+      favicon.href = '/assets/images/logos/logo-icon-dark.svg'; 
+    } else {
+      favicon.href = '/assets/images/logos/logo-icon-light.svg'; 
+    }
+  }, [currentTheme]);
+
   return (
     <div className="flex items-center">
       <button
         data-testid="switch-theme-btn"
         onClick={() => toggleTheme(currentTheme === 'light' ? 'dark' : 'light')}
-        className="icon-language rounded-full cursor-pointer focus:outline-none flex items-center text-nav-text  g:text-nav-text hover:text-nav-hover"
+        className="icon-language rounded-full cursor-pointer focus:outline-none flex items-center text-nav-text g:text-nav-text hover:text-nav-hover"
       >
         <FontAwesomeIcon
           icon={currentTheme === 'light' ? faSun : faMoon}
