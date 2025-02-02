4b2689647e4a31b5b7caaa45fdc223c6b288858c
fianerd <mattiasson.sofia@gmail.com>
2025-02-02 21:29:55 +0100

    Renamed folder to remove invalid characters

--- src/component/footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-      <p className='font-heading bg-accent-primary dark:bg-bg-primary text-hover-text dark:text-text-secondary text-sm text-center py-2 mb-0'>
-          &copy; FiaNerd | Sofia Mattiasson
-      </p>
-  )
-}
-
-export default Footer
