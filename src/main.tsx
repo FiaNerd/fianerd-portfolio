dfb9936e9f7970d3e28f4b638417e34ae5bcbe47
fianerd <mattiasson.sofia@gmail.com>
2025-02-03 21:06:43 +0100

    Fixed config file for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,21 @@
-// src/main.tsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-import { I18nextProvider } from 'react-i18next'
-import { BrowserRouter } from 'react-router-dom'
-import '../public/assets/i18n/i18n.ts'
-import translation from '../public/assets/i18n/i18n.ts'
-import { ThemeProvider } from './context/ThemeContext.tsx'
-import App from './App'
+import { I18nextProvider } from 'react-i18next';
+import { BrowserRouter } from 'react-router-dom';
+import i18n from '../public/assets/i18n/i18n';
+import { ThemeProvider } from './context/ThemeProvider';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-      <ThemeProvider>
-    <I18nextProvider i18n={translation}>
+    <ThemeProvider>
+      <I18nextProvider i18n={i18n}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-    </I18nextProvider>
-      </ThemeProvider>
+      </I18nextProvider>
+    </ThemeProvider>
   </React.StrictMode>
-)
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
   },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
   build: {
     rollupOptions: {
+      input: '/src/main.tsx',
       output: {
         manualChunks: {
           'react-vendor': ['react', 'react-dom'],
