3bc002592d0b8bdee8f8d4523003eb13e26ccb96
fianerd <mattiasson.sofia@gmail.com>
2025-02-03 23:51:41 +0100

    Fixed issue with the home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,12 +2,12 @@ import { useLayoutEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import HeroSection from "../component/heroSection/HeroSection";
 import Hobbies from "../component/hobbies/Hobbies";
-import Title from "../component/partial/Title";
-import AboutMe from "../component/profile/about/AboutMe";
-import Education from "../component/profile/education/Education";
-import WorkExperience from "../component/profile/experience/WorkExperience";
-import Skills from "../component/profile/skills/Skills";
 import { useSmoothScroll } from "../hook/useSmoothScroll";
+import AboutMe from "../component/Profile/about/AboutMe";
+import Education from "../component/Profile/education/Education";
+import WorkExperience from "../component/Profile/experience/WorkExperience";
+import Skills from "../component/Profile/skills/Skills";
+import Title from "../component/partial/Title";
 
 const HomePage = () => {
   const [headerHeight, setHeaderHeight] = useState(0);
