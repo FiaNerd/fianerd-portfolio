4e9aa632d8babd68eee89682a9b6543caeb2e662
FiaNerd <67582718+FiaNerd@users.noreply.github.com>
2025-02-05 20:48:33 +0100

    Add error fallback again

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { ErrorBoundary } from 'react-error-boundary';
 import { Route, Routes } from 'react-router-dom';
 import Header from './component/header/Header';
+import ErrorFallback from './component/partial/errors/ErrorFallBack';
+import SocialMediaAndContact from './component/SocialMediaAndContact';
 import HomePage from './pages/HomePage';
 import PortfolioPage from './pages/portfolio/PortfolioPage';
 
@@ -10,14 +13,22 @@ const App = () => {
       <div className="flex flex-grow">
         {/* Sidebar */}
         <div className="sticky top-0 h-screen bg-bg-secondary text-text-secondary dark:bg-black dark:text-text-accent z-50 pointer-events-auto">
-          {/* Temporarily remove SocialMediaAndContact */}
+          <SocialMediaAndContact />
         </div>
         {/* Main Content */}
         <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/portfolio" element={<PortfolioPage />} />
-          </Routes>
+          <ErrorBoundary
+            FallbackComponent={ErrorFallback}
+            onReset={() => {
+              // Reset the state of your app so the error doesn't happen again
+              window.location.reload();
+            }}
+          >
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/portfolio" element={<PortfolioPage />} />
+            </Routes>
+          </ErrorBoundary>
         </main>
       </div>
     </div>
