f8f8047263347a1fe08c448c6187533503916467
FiaNerd <67582718+FiaNerd@users.noreply.github.com>
2025-02-05 20:44:53 +0100

    Test to delete error fallback again

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,26 @@
-import { ErrorBoundary } from 'react-error-boundary';
 import { Route, Routes } from 'react-router-dom';
 import Header from './component/header/Header';
-import ErrorFallback from './component/partial/errors/ErrorFallBack';
-import SocialMediaAndContact from './component/SocialMediaAndContact';
 import HomePage from './pages/HomePage';
 import PortfolioPage from './pages/portfolio/PortfolioPage';
 
 const App = () => {
   return (
     <div className="App">
-    <Header />
-    <div className="flex flex-grow">
-      {/* Sidebar */}
-      <div className="sticky top-0 h-screen bg-bg-secondary text-text-secondary dark:bg-black dark:text-text-accent z-50 pointer-events-auto">
-        <SocialMediaAndContact />
-      </div>
-      {/* Main Content */}
-      <main className="flex-grow">
-        <ErrorBoundary
-          FallbackComponent={ErrorFallback}
-          onReset={() => {
-            // Reset the state of your app so the error doesn't happen again
-            window.location.reload();
-          }}
-        >
+      <Header />
+      <div className="flex flex-grow">
+        {/* Sidebar */}
+        <div className="sticky top-0 h-screen bg-bg-secondary text-text-secondary dark:bg-black dark:text-text-accent z-50 pointer-events-auto">
+          {/* Temporarily remove SocialMediaAndContact */}
+        </div>
+        {/* Main Content */}
+        <main className="flex-grow">
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/portfolio" element={<PortfolioPage />} />
           </Routes>
-        </ErrorBoundary>
-      </main>
+        </main>
+      </div>
     </div>
-  </div>
   );
 };
 
