569448031027181b05dd73d6ee537da6fb02f999
FiaNerd <67582718+FiaNerd@users.noreply.github.com>
2025-02-05 22:23:36 +0100

    Fixxed errror in the eslintrc file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     sourceType: 'module',
     project: ['./tsconfig.json', './tsconfig.node.json'], 
     tsconfigRootDir: __dirname, 
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
