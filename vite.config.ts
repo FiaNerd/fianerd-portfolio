8d44800895e9bb56e981565a5014813b5e4d8fe8
FiaNerd <67582718+FiaNerd@users.noreply.github.com>
2025-02-08 15:10:26 +0100

    Build project

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "./", // Ensures relative paths are used
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom']
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000
-  }
+    outDir: "dist",
+  },
 });
