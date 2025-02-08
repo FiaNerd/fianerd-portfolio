816c893b78ebd24be9563ab0ad6e21844cf39285
FiaNerd <67582718+FiaNerd@users.noreply.github.com>
2025-02-08 15:17:28 +0100

    Build project

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
-// vite.config.ts
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "./", // Ensures relative paths are used
+  base: "/", // Use this for Vercel deployments
   plugins: [react()],
   build: {
     outDir: "dist",
-  },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom"]
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  }
 });
