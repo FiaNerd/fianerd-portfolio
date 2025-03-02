7910d06691da77f9adf61a91e0640d6dc5341039
FiaNerd <67582718+FiaNerd@users.noreply.github.com>
2025-03-02 20:46:38 +0100

    Add download resume

--- src/components/heroSection/HeroSection.tsx
+++ src/components/heroSection/HeroSection.tsx
@@ -1,6 +1,7 @@
 import { Icon } from "@iconify/react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
+import resume from "../../../public/assets/files/Resume.pdf";
 import Button from "../partials/Button";
 import ResponsiveHeroImage from "./ResponsiveHeroImage";
 
@@ -23,9 +24,10 @@ const HeroSection = () => {
             <p className="text-[#350712] dark:text-text-primary text-sm sm:text-base md:text-md mb-4 lg:mb-8">{t("text")}</p>
           
             <div className="flex flex-col md:flex-row gap-8 w-full">
-              <Button className="flex flex-row gap-2 justify-center items-center bg-btn-bg text-bg-primary border-2 border-btn-bg hover:border-bg-hover hover:bg-bg-hover font-semibold tracking-wide">
-              <Icon icon="line-md:cloud-alt-download" width="30" height="30" /> {t("ctaButtonCV")}
+              <Button className="flex flex-row gap-2 justify-center items-center bg-btn-bg text-bg-primary border-2 border-btn-bg hover:bg-bg-hover hover:border-[#350712ab] font-semibold tracking-wide">
+              <a href={resume} download="resume">{t("ctaButtonCV")}</a>
               </Button>
+
               <Button 
                 onClick={() => navigate('/portfolio')} 
                 className="flex flex-row gap-2 justify-center items-center border-2 border-[#350712] bg-[#350712] dark:bg-transparent dark:border-btn-bg text-bg-primary dark:text-btn-bg tracking-wide font-semibold hover:bg-bg-hover hover:border-[#350712ab] hover:text-bg-primary dark:hover:text-bg-primary dark:hover:bg-bg-hover"
