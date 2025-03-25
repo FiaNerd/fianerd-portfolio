42a972f34fa323515c5260e82d5d50023e18d525
mattiasson.sofia@icloud.com <mattiasson.sofia@icloud.com>
2025-03-25 09:19:54 +0100

    Fixed image layout for the gallery

--- src/components/portfolios/GraphicImageDetails.tsx
+++ src/components/portfolios/GraphicImageDetails.tsx
@@ -21,23 +21,51 @@ const GraphicImageDetails = () => {
     urlTitle: string;
   }[];
 
-  console.log(graphicImages);
-
   return (
-    <div>
+    <div className="p-4 flex justify-center">
       <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
-        {graphicImages.map((image, index) => (
-          <a
-            key={index}
-            href={image.image}
-            data-sub-html={`<h4>${image.title}</h4><p>${image.description}</p>`}
-          >
-            <img src={image.image} alt={image.title} />
-          </a>
-        ))}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {/* Kolumn 1 (3 bilder, förskjutna) */}
+          <div className="grid md:grid-rows-2 lg:grid-rows-3 gap-4 lg:place-self-center">
+            <ImageItem image={graphicImages[0]} />
+            <ImageItem image={graphicImages[1]} />
+            <ImageItem image={graphicImages[2]} />
+          </div>
+
+          {/* Kolumn 2 (4 bilder, huvudkolumnen) */}
+          <div className="grid md:grid-rows-2 lg:grid-rows-4 gap-4">
+            <ImageItem image={graphicImages[3]} />
+            <ImageItem image={graphicImages[4]} />
+            <ImageItem image={graphicImages[5]} />
+            <ImageItem image={graphicImages[6]} />
+          </div>
+
+          {/* Kolumn 3 (3 bilder, förskjutna) */}
+          <div className="grid md:grid-rows-2 lg:grid-rows-3 gap-4 lg:place-self-center">
+            <ImageItem image={graphicImages[7]} />
+            <ImageItem image={graphicImages[8]} />
+            <ImageItem image={graphicImages[9]} />
+          </div>
+        </div>
       </LightGallery>
     </div>
   );
 };
+
+// Bildkomponent
+const ImageItem = ({ image }: { image: any }) => (
+  <a
+    href={image.image}
+    data-src={image.image}
+    data-sub-html={`<h4>${image.title}</h4><p>${image.description}</p>`}
+    className="cursor-zoom-in block w-full h-auto hover:scale-105 transition-transform duration-300 ease-in-out"
+  >
+    <img
+      src={image.image}
+      alt={image.title}
+      className="w-full h-full object-cover shadow-md"
+    />
+  </a>
+);
 
 export default GraphicImageDetails;
