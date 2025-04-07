3c573153aa4bda38863f0217e8055d5e1cad00e5
mattiasson.sofia@icloud.com <mattiasson.sofia@icloud.com>
2025-04-07 23:01:09 +0200

    Changed my resume and added cover letter

--- src/components/heroSection/HeroSection.tsx
+++ src/components/heroSection/HeroSection.tsx
@@ -1,51 +1,66 @@
-import { Icon } from "@iconify/react";
-import { useTranslation } from "react-i18next";
-import { useNavigate } from "react-router-dom";
-import i18n from "../../../public/i18n/i18n";
-import Button from "../partials/Button";
-import ResponsiveHeroImage from "./ResponsiveHeroImage";
+import { Icon } from '@iconify/react';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import i18n from '../../../public/i18n/i18n';
+import Button from '../partials/Button';
+import ResponsiveHeroImage from './ResponsiveHeroImage';
 
 const HeroSection = () => {
-  const { t } = useTranslation("heroSection");
+  const { t } = useTranslation('heroSection');
   const navigate = useNavigate();
 
   const onButtonClick = () => {
-    const resumePath = i18n.language === "sv" ? "/files/SofiaMattiasson-CV-sv.pdf" : "/files/SofiaMattiasson-Resume-en.pdf";
-    const link = document.createElement("a");
+    const resumePath =
+      i18n.language === 'sv'
+        ? '/files/Sofia-Mattiasson-CV-PB-sv.pdf'
+        : '/files/Sofia-Mattiasson-Resume-Coverletter-en.pdf';
+    const link = document.createElement('a');
     link.href = resumePath;
-    link.download = i18n.language === "sv" ? "SofiaMattiasson-CV-sv.pdf" : "SofiaMattiasson-Resume-en.pdf";
+    link.download =
+      i18n.language === 'sv'
+        ? 'Sofia-Mattiasson-CV-PB-sv.pdf'
+        : 'Sofia-Mattiasson-Resume-Coverletter-en.pdf';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-}
+  };
 
   return (
     <div className="relative w-full h-full flex justify-center">
       <div className="absolute grid grid-cols-1 md:grid-cols-[80%] lg:grid-cols-[65%] 2xl:grid-cols-[50%] w-full h-full z-10">
         <div className="flex flex-col justify-center items-start px-6 md:px-12 text-left md:text-left z-10 w-full h-full">
-          <div className={"bg-white/30 dark:bg-black/40 p-6 rounded"}>
+          <div className={'bg-white/30 dark:bg-black/40 p-6 rounded'}>
             <h1
               className="text-[#350712] dark:text-text-accent text-2xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-4xl mb-4 font-bold leading-snug lg:mb-12"
-              dangerouslySetInnerHTML={{ __html: t("title") }}
+              dangerouslySetInnerHTML={{ __html: t('title') }}
             />
             <h2 className="text-[#5c1e64] dark:text-accent-primary text-xl sm:text-2xl md:text-2xl lg:text-3xl mb-0">
-              {t("subTitle")}
+              {t('subTitle')}
             </h2>
             <h3 className="text-[#350712]/80 dark:text-text-accent text-lg sm:text-xl md:text-xl mb-4">
-              {t("textIntro")}
+              {t('textIntro')}
             </h3>
             <p className="text-[#350712] dark:text-text-primary text-sm sm:text-base md:text-md mb-4 lg:mb-8">
-              {t("text")}
+              {t('text')}
             </p>
             <div className="flex flex-col md:flex-row gap-8 w-full">
-              <Button onClick={onButtonClick} className="flex flex-row gap-2 justify-center items-center bg-btn-bg text-bg-primary border-2 border-btn-bg hover:border-bg-hover hover:bg-bg-hover font-semibold tracking-wide">
-                <Icon icon="line-md:cloud-alt-download" width="30" height="30" /> {t("ctaButtonCV")}
+              <Button
+                onClick={onButtonClick}
+                className="flex flex-row gap-2 justify-center items-center bg-btn-bg text-bg-primary border-2 border-btn-bg hover:border-bg-hover hover:bg-bg-hover font-semibold tracking-wide"
+              >
+                <Icon
+                  icon="line-md:cloud-alt-download"
+                  width="30"
+                  height="30"
+                />{' '}
+                {t('ctaButtonCV')}
               </Button>
-              <Button 
-                onClick={() => navigate('/portfolio')} 
+              <Button
+                onClick={() => navigate('/portfolio')}
                 className="flex flex-row gap-2 justify-center items-center border-2 border-[#350712] bg-[#350712] dark:bg-transparent dark:border-btn-bg text-bg-primary dark:text-btn-bg tracking-wide font-semibold hover:bg-bg-hover hover:border-[#350712ab] hover:text-bg-primary dark:hover:text-bg-primary dark:hover:bg-bg-hover"
               >
-                <Icon icon="ix:explore" width="30" height="30" /> {t("ctaButtonPortfolio")}
+                <Icon icon="ix:explore" width="30" height="30" />{' '}
+                {t('ctaButtonPortfolio')}
               </Button>
             </div>
           </div>
--- src/pages/portfolios/PortfolioPage.tsx
+++ src/pages/portfolios/PortfolioPage.tsx
@@ -38,14 +38,14 @@ const PortfolioPage = () => {
       'graphic-design',
     ],
     'portfolio',
-    headerHeight,
+    headerHeight
   );
 
   const onButtonClick = () => {
     const resumePath =
       i18n.language === 'sv'
-        ? '/files/SofiaMattiasson-CV-sv.pdf'
-        : '/files/SofiaMattiasson-Resume-en.pdf';
+        ? '/files/Sofia-Mattiasson-CV-PB-sv.pdf'
+        : '/files/Sofia-Mattiasson-Resume-Coverletter-en.pdf';
 
     const link = document.createElement('a');
 
@@ -53,8 +53,8 @@ const PortfolioPage = () => {
 
     link.download =
       i18n.language === 'sv'
-        ? 'SofiaMattiasson-CV-sv.pdf'
-        : 'SofiaMattiasson-Resume-en.pdf';
+        ? 'Sofia-Mattiasson-CV-PB-sv.pdf'
+        : 'Sofia-Mattiasson-Resume-Coverletter-en.pdf';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -68,58 +68,57 @@ const PortfolioPage = () => {
         transition: 'top 0.3s ease',
       }}
     >
-        <section className="bg-[#436e74] dark:bg-[#16443e]">
-          <Title
-            id="portfolio"
-            title={t('titlePortfolio')}
-            dot={'.'}
-            children={t('subTitlePortfolio')}
-            className="bg-[#436e74] dark:bg-[#16443e] text-[#f39058] dark:text-[#d85f2d] mb-6 md:mb-8 lg:mb-10 leading-[0]"
-            sticky
-          />
-          <div className="px-4 md:px-12 py-8 grid grid-row-1 lg:grid-cols-[50%_40%] 2xl:grid-cols-[40%_30%] gap-8 lg:gap-20 items-center justify-center">
-            <div className="flex flex-col justify-center items-center text-center gap-4 md:gap-8">
-              <div className="flex flex-col lg:gap-8 md:border-double md:border-4 py-8 px-0 md:px-8 border-[#f3d758] dark:border-[#d85f2d] rounded">
-                <h1 className="text-[#f3d758] dark:text-[#d85f2d] text-md md:text-[3rem] xl:text-[5rem] xl:leading-[4.2rem] font-semibold tracking-wpide">
-                  {t('mainSectionTitle').toUpperCase()}
-                </h1>
-                <p className="font-semibold tracking-wide">
-                  {t('portfolioIntro')}
-                </p>
-                <div className="flex flex-col w-full xl:flex-row items-center mx-auto md:mt-8 gap-2">
-                  <Button
-                    onClick={onButtonClick}
-                    className="flex flex-row justify-center items-center gap-4 bg-btn-bg text-bg-primary border-btn-bg hover:border-bg-hover hover:bg-bg-hover font-semibold tracking-wide"
-                  >
-                    <Icon
-                      icon="line-md:cloud-alt-download"
-                      width="30"
-                      height="30"
-                    />{' '}
-                    {t('ctaButtonCV')}
-                  </Button>
-                  <NavLink
-                    to={'/contact'}
-                    className="flex flex-row w-full justify-center items-center gap-4 font-sub-heading bg-[#350712] border-3 border-[#350712] text-bg-primary dark:text-text-primary tracking-wide font-semibold hover:bg-bg-hover dark:hover:bg-[#350712ab] hover:border-bg-hover dark:hover:border-[#350712ab] hover:text-bg-primary  rounded text-base md:text-md lg:text-lg hover:shadow-lg px-4 py-2 cursor-pointer"
-                  >
-                    <Icon icon="line-md:email-twotone" width="30" height="30" />{' '}
-                    {t('ctaButtonContactMe')}
-                  </NavLink>
-                </div>
+      <section className="bg-[#436e74] dark:bg-[#16443e]">
+        <Title
+          id="portfolio"
+          title={t('titlePortfolio')}
+          dot={'.'}
+          children={t('subTitlePortfolio')}
+          className="bg-[#436e74] dark:bg-[#16443e] text-[#f39058] dark:text-[#d85f2d] mb-6 md:mb-8 lg:mb-10 leading-[0]"
+          sticky
+        />
+        <div className="px-4 md:px-12 py-8 grid grid-row-1 lg:grid-cols-[50%_40%] 2xl:grid-cols-[40%_30%] gap-8 lg:gap-20 items-center justify-center">
+          <div className="flex flex-col justify-center items-center text-center gap-4 md:gap-8">
+            <div className="flex flex-col lg:gap-8 md:border-double md:border-4 py-8 px-0 md:px-8 border-[#f3d758] dark:border-[#d85f2d] rounded">
+              <h1 className="text-[#f3d758] dark:text-[#d85f2d] text-md md:text-[3rem] xl:text-[5rem] xl:leading-[4.2rem] font-semibold tracking-wpide">
+                {t('mainSectionTitle').toUpperCase()}
+              </h1>
+              <p className="font-semibold tracking-wide">
+                {t('portfolioIntro')}
+              </p>
+              <div className="flex flex-col w-full xl:flex-row items-center mx-auto md:mt-8 gap-2">
+                <Button
+                  onClick={onButtonClick}
+                  className="flex flex-row justify-center items-center gap-4 bg-btn-bg text-bg-primary border-btn-bg hover:border-bg-hover hover:bg-bg-hover font-semibold tracking-wide"
+                >
+                  <Icon
+                    icon="line-md:cloud-alt-download"
+                    width="30"
+                    height="30"
+                  />{' '}
+                  {t('ctaButtonCV')}
+                </Button>
+                <NavLink
+                  to={'/contact'}
+                  className="flex flex-row w-full justify-center items-center gap-4 font-sub-heading bg-[#350712] border-3 border-[#350712] text-bg-primary dark:text-text-primary tracking-wide font-semibold hover:bg-bg-hover dark:hover:bg-[#350712ab] hover:border-bg-hover dark:hover:border-[#350712ab] hover:text-bg-primary  rounded text-base md:text-md lg:text-lg hover:shadow-lg px-4 py-2 cursor-pointer"
+                >
+                  <Icon icon="line-md:email-twotone" width="30" height="30" />{' '}
+                  {t('ctaButtonContactMe')}
+                </NavLink>
               </div>
             </div>
-            <div className="flex justify-center items-center">
-              <img
-                src="/assets/images/portfolio/me-img.webp"
-                alt="portfolio"
-                className="w-full h-auto object-cover rounded-lg mb-4"
-              />
-            </div>
           </div>
-        </section>
+          <div className="flex justify-center items-center">
+            <img
+              src="/assets/images/portfolio/me-img.webp"
+              alt="portfolio"
+              className="w-full h-auto object-cover rounded-lg mb-4"
+            />
+          </div>
+        </div>
+      </section>
 
-        <SvgWaves colorLight={'#436e74'} colorDark={'#16443e'} />
-
+      <SvgWaves colorLight={'#436e74'} colorDark={'#16443e'} />
 
       <section id="top-5-projects" className="top-0">
         <Title
