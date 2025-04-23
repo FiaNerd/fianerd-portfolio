370a1bd08f1ab79b9e163cea2e0360d948b71ac6
mattiasson.sofia@icloud.com <mattiasson.sofia@icloud.com>
2025-04-23 08:03:06 +0200

    Fixed buttons on portfolio section to get better responsive layout

--- src/components/portfolios/PortfolioHeroIntro.tsx
+++ src/components/portfolios/PortfolioHeroIntro.tsx
@@ -26,14 +26,14 @@ const PortfolioHeorIntro = () => {
   };
 
   return (
-    <div className="max-w-screen-5xl mx-auto px-4 md:px-12 py-8 grid grid-row-1 lg:grid-cols-[50%_40%] 2xl:grid-cols-[40%_30%] gap-8 lg:gap-20 items-center justify-center">
+    <div className="max-w-screen-5xl mx-auto px-4 md:px-12 py-8 grid grid-row-1 lg:grid-cols-[50%_40%] 2xl:grid-cols-[60%_30%] gap-8 lg:gap-20 items-center justify-center">
       <div className="flex flex-col justify-center items-center text-center gap-4 md:gap-8">
         <div className="flex flex-col lg:gap-8 md:border-double md:border-4 py-8 px-0 md:px-8 border-[#f3d758] dark:border-[#d85f2d] rounded">
           <h1 className="text-[#f3d758] dark:text-[#d85f2d] text-md md:text-[3rem] xl:text-[5rem] xl:leading-[4.2rem] font-semibold tracking-wpide">
             {t('mainSectionTitle').toUpperCase()}
           </h1>
           <p className="font-semibold tracking-wide">{t('portfolioIntro')}</p>
-          <div className="flex flex-col w-full xl:flex-row items-center mx-auto md:mt-8 gap-2">
+          <div className="flex flex-col w-full xl:flex-row items-center mx-auto md:mt-8 gap-4 md:gap-8 xl:gap-16">
             <Button
               onClick={onButtonClick}
               className="flex flex-row justify-center items-center gap-4 bg-btn-bg text-bg-primary border-btn-bg hover:border-bg-hover hover:bg-bg-hover font-semibold tracking-wide"
